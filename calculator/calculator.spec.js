653c82c1ac6c1c2c4c78cfa09bed5e35e238fd0c
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-08 12:10:23 -0500

    First tests running

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,25 @@
-const { add } = require('./calculator.js');
+const { add } = require("./calculator.js");
+
+function honest() {
+  return true;
+}
+
+test("an honest function returns true", () => {
+  expect(honest()).toBeTruthy();
+});
 
 // test away!
+test("should add 2 and 2 to equal 4", () => {
+  //Arrange
+  const addMock = jest.fn(add);
+  const expected = 4;
+  const val1 = 2;
+  const val2 = 2;
+
+  //Act
+  const sum = addMock(val1, val2);
+
+  //Assert
+  expect(addMock).toHaveBeenCalledTimes(1);
+  expect(sum).toEqual(expected);
+});
